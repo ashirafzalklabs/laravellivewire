4a9805247027a4057f1c5f5303c8802e6327e31c
ashirafzakklabs <ashirafzal.klabs@gmail.com>
2021-01-14 13:59:40 +0500

    live wire set up with laravel.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit1c415eec43714138398a68fb60c4db03::getLoader();
+return ComposerAutoloaderInitec4e1e41e125e4a4960d59a322d2961b::getLoader();

5e32f73caec48f82a205610aa98b2b494361f55c
ashirafzakklabs <ashirafzal.klabs@gmail.com>
2021-01-14 16:43:12 +0500

    live wire counter component.

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div style="text-align: center; margin:1rem;">
+    <button wire:click="increment" type="button" class="btn btn-primary">+</button>
+    <h1>Counter</h1>
+    <h1>{{$count}}</h1>
+    <button wire:click="decrement" type="button" class="btn btn-danger">-</button>
+</div>
